97da30f4298b2a03ca81dd04ea3480dc69b8e1af
V123654M <qsxcvfrtg.2hft6.m@gmail.com>
2024-06-18 06:43:26 +0330

    Delete app-common-io directory

--- app-common-io/src/main/java/eu/darken/sdmse/common/pkgs/pkgops/PkgOpsException.kt
+++
@@ -1,8 +0,0 @@
-package eu.darken.sdmse.common.pkgs.pkgops
-
-import java.io.IOException
-
-open class PkgOpsException(
-    message: String? = null,
-    cause: Throwable? = null
-) : IOException(message, cause)
